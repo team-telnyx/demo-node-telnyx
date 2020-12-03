a66a93d79db6b7cd2fba9a1aa8791660b7c8c96b
Jack Gordley <jack@telnyx.com>
2020-12-03 15:53:07 -0500

    express-2fa-demo: Added MySQL support and working endpoints

--- express-2fa/index.js
+++ express-2fa/index.js
@@ -6,10 +6,28 @@ require('dotenv').config()
 // Library Requirements
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const mysql = require('mysql');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const Connections = require('telnyx/lib/resources/Connections');
 
 // Set up telnyx library with user API Key
 const telnyx = require('telnyx')(process.env.TELNYX_API_KEY);
+
+const connection = mysql.createConnection({
+  host: process.env.DB_SERVER_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+connection.connect((err) => {
+  if (err) {
+    console.log("Error connecting to Db");
+    return;
+  }
+  console.log('Connected!');
+});
 
 // Fire up express app and settings
 const app = express();
@@ -17,47 +35,99 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 // Async function to initiate a new verification request
 const create2FA = async (number) => {
-  //TODO: Call Telnyx API to create a new 2FA with given number
-  // try {
-  //   const verify_request = {
-  //     phone_number: number,
-  //     twofa_profile_id: process.env.TELNYX_TWOFA_PROFILE,
-  //     type: "sms",
-  //     timeout: 300
-  //   };
-  //   const telnyxResponse = await telnyx.verifications.create(verify_request);
-  //   console.log(`Verification created with id: ${telnyxResponse.data.id}`);
-  // }
-  // catch(e) {
-  //   console.log('Error creating verification');
-  //   console.log(e);
-  //   return false;
-  // }
+  //Call Telnyx API to create a new 2FA with given number
+  try {
+    const headers = {
+      "Authorization": "Bearer " + process.env.TELNYX_API_KEY,
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+    const verify_request = {
+      phone_number: number,
+      twofa_profile_id: process.env.TELNYX_VERIFY_KEY,
+      type: "sms",
+      timeout: 300
+    };
+    const url = "https://api.telnyx.com/v2/verifications";
+
+    const telnyxResponse = await axios(
+      {
+        method: 'post',
+        url: url,
+        headers: headers,
+        data: verify_request
+      }
+    );
+    console.log('Verification created with id: ', telnyxResponse.data.data.id);
+  }
+  catch(e) {
+    console.log('Error creating verification');
+    console.log(e);
+    return false;
+  }
   return true;
 }
 
 // Async function to initiate a new verification submission with code
-const verify2FA = async (code) => {
-  //TODO: Call Telnyx API to verify input code
-  // try {
-  //   const verify_request = {
-  //     phone_number: number,
-  //     code: code
-  //   };
-  //   const telnyxResponse = await telnyx.verifications.submit(verify_request);
-  //   console.log(`Successfully verified: ${telnyxResponse.data.id}`);
-  // }
-  // catch(e) {
-  //   console.log('Error creating verification');
-  //   console.log(e);
-  //   return false;
-  // }
+const verify2FA = async (number, code) => {
+  //Call Telnyx API to submit a verify code
+  try {
+    const headers = {
+      "Authorization": "Bearer " + process.env.TELNYX_API_KEY,
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    };
+    const verify_request = {
+      code: code
+    };
+    const url = "https://api.telnyx.com/v2/verifications/by_phone_number/" + number + "/actions/verify"
+
+    const telnyxResponse = await axios(
+      {
+        method: 'post',
+        url: url,
+        headers: headers,
+        data: verify_request
+      }
+    );
+    console.log('Verification success with response: ', telnyxResponse.data.data.id);
+  }
+  catch(e) {
+    console.log('Incorrect Verfication!');
+    console.log(e);
+    return false;
+  }
   return true;
 }
+
+// Async function to initiate a new verification submission with code
+const insertUser = async (name, password, number) => {
+  const query = 'INSERT INTO `users`(`username`, `password`, `phone`, `verified`) VALUES (?, ?, ?, 0)';
+  connection.query(query, [name, password, number], (err, result) => {
+    if (err) throw err;
+
+    console.log(`Changed ${result.changedRows} row(s)`);
+  });
+}
+
+const verifyUser = async (name) => {
+  const query = 'UPDATE `users` SET `verified`=1 WHERE `username`=?';
+  connection.query(query, [name], (err, result) => {
+    if (err) throw err;
+
+    console.log(`Changed ${result.changedRows} row(s)`);
+  });
+}
+
+// Simple sign up page with built in post request
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname+'/templates/signup.html'));
+});
 
 
 // Simple sign up page with built in post request
@@ -78,11 +148,13 @@ app.post('/auth', async(request, response) => {
   var password = request.body.password;
   var phone = request.body.number;
 
+  insertUser(username, password, phone);
+
   const telnyx_response = await create2FA(phone);
 
   // If the verification was successfully created then we move to verify
 	if (response) {
-		response.sendFile(path.join(__dirname+'/templates/verify.html'));
+    response.cookie('username', username).cookie('number', phone).sendFile(path.join(__dirname+'/templates/verify.html'));
 	} else {
 		response.send('There was an error generating a validation');
 		response.end();
@@ -91,10 +163,12 @@ app.post('/auth', async(request, response) => {
 
 // Verify page retrieves try from post request and validates it
 app.post('/verify', async(request, response) => {
+  console.log('Cookies: ', request.cookies);
   const code_try = request.body.code;
-  const telnyx_response = await verify2FA(code_try);
+  const telnyx_response = await verify2FA(request.cookies.number, code_try);
+  verifyUser(request.cookies.username);
 	if (response) {
-		response.send('Phone <b>verified</b>. Thank you!');
+		response.sendFile(path.join(__dirname+'/templates/welcome.html'));
 	} else {
 		response.send('Incorrect pin, please try again!');
 		response.end();
