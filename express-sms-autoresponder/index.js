8b73278d4e41121fff4fa08b76feefabc393c884
Drew Kitch <drew@Drews-MacBook-Pro.local>
2021-05-27 12:35:13 -0600

    async/await

--- express-sms-autoresponder/index.js
+++ express-sms-autoresponder/index.js
@@ -1,55 +1,48 @@
-require('dotenv').config()
-const telnyxSMSNumber = process.env.TELNYX_SMS_NUMBER
-const telnyxApiKey = process.env.TELNYX_API_KEY
-const telnyx = require('telnyx')(telnyxApiKey)
+require('dotenv').config();
+const telnyxSMSNumber = process.env.TELNYX_SMS_NUMBER;
+const telnyxApiKey = process.env.TELNYX_API_KEY;
+const telnyx = require('telnyx')(telnyxApiKey);
 
-const express = require('express')
-const app = express()
-const port = 5000
-app.use(express.json())
+const express = require('express');
+const app = express();
+const port = 5000;
+app.use(express.json());
 
 const preparedReplies = new Map([
   ['pizza', 'Chicago pizza is the best'],
   ['ice cream', 'I prefer gelato']
-])
-const defaultReply = "Please send either the word 'pizza' or 'ice cream' for a different response"
+]);
+const defaultReply = "Please send either the word 'pizza' or 'ice cream' for a different response";
 
 const processWebhook = (webhookBody) => {
-  const eventType = webhookBody['data']['event_type']
-  const payload = webhookBody['data']['payload']
-  const direction = payload['direction']
+  const eventType = webhookBody['data']['event_type'];
+  const payload = webhookBody['data']['payload'];
+  const direction = payload['direction'];
 
   if (eventType === 'message.received' && direction === 'inbound') {
-    const smsMessage = payload['text'].replace(/\s+/g, ' ').trim().toLowerCase()
-    const replyToTN = payload['from']['phone_number']
-    const preparedReply = preparedReplies.get(smsMessage) || defaultReply
-    console.log(preparedReply)
+    const smsMessage = payload['text'].replace(/\s+/g, ' ').trim().toLowerCase();
+    const replyToTN = payload['from']['phone_number'];
+    const preparedReply = preparedReplies.get(smsMessage) || defaultReply;
 
-    telnyx.messages.create(
-      {
-        'from': telnyxSMSNumber,
-        'to': replyToTN,
-        'text': preparedReply
-      },
-      (err, res) => {
-        if (err) {
-          console.log(err)
-        }
-      }
-    )
+    return telnyx.messages.create({
+      'from': telnyxSMSNumber,
+      'to': replyToTN,
+      'text': preparedReply
+    });
   }
-}
+};
 
-app.post('/webhooks', (req, res) => {
+app.post('/webhooks', async (req, res) => {
   try {
-    processWebhook(req.body)
+    const result = await processWebhook(req.body);
+    console.log(result);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   } finally {
-    res.status(200).end()
+    res.status(200).end();
   }
-})
+});
 
 app.listen(port, () => {
-  console.log(`Telnyx SMS autoresponder quickstart app is listening at http://localhost:${port}`)
-})
+  console.log(`Telnyx SMS autoresponder quickstart app is listening at http://localhost:${port}`);
+});
