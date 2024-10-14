57ff640101d9cf4ad20657278ec60503625d1eee
Lucas Rosa <lucas@telnyx.com>
2024-10-14 14:52:32 -0300

    use texml translator

--- express-texml-call-mask/controllers/dtmfDialController.ts
+++ express-texml-call-mask/controllers/dtmfDialController.ts
@@ -1,77 +1,69 @@
 import express from "express";
-import * as db from "../models/db";
-import twilio from "twilio";
 import Telnyx from "telnyx";
+import * as db from "../models/db";
 import * as texml from "../packages/texml";
 
 const router = express.Router();
-const VoiceResponse = twilio.twiml.VoiceResponse;
 
-const hangupSentence = "Thank you for the call, hanging up";
+type TelnyxTexmlTranslatorGatherCallback = {
+  AccountSid: string;
+  CallSid: string;
+  CallSidLegacy: string;
+  Digits: string;
+  From: string;
+  To: string;
+};
 
-router.route("/inbound").post(async (req, res) => {
-  const defaultGatherSentence =
-    "Hello, please enter the phone number with country code you would like to dial followed by the pound sign";
-  const event = req.body as Telnyx.events.CallAnsweredEvent;
-  console.log(event);
-  const response = new VoiceResponse();
-  const gather = response.gather({
-    action: "gather",
-    method: "POST",
-    numDigits: 10,
-    finishOnKey: "#",
+router
+  .route("/inbound")
+  .post<{}, unknown, Telnyx.events.CallEvent>(async (req, res) => {
+    const gatherSentence =
+      "Hello, please enter the phone number with country code you would like to dial followed by the pound sign";
+    const event = req.body;
+    console.log(event);
+
+    res.type("application/xml");
+    res.send(texml.gatherTeXML(gatherSentence, "#", 10, 15));
   });
-  gather.say(defaultGatherSentence);
-  response.say(hangupSentence);
-  response.hangup();
-  res.type("application/xml");
-  res.send(response.toString());
-});
 
-router.route("/gather").post(async (req, res) => {
-  const event = req.body as Telnyx.events.CallGatherEndedEvent;
-  console.log(event);
-  const phoneNumber = event.data?.payload?.digits;
-  const userRecord = db.lookupUserByPSTNPhoneNumber(event.data?.payload?.from!);
+router
+  .route("/gather")
+  .post<{}, unknown, TelnyxTexmlTranslatorGatherCallback>(async (req, res) => {
+    const event = req.body;
+    console.log(event);
+    const phoneNumber = event.Digits;
+    const userRecord = db.lookupUserByPSTNPhoneNumber(event.From);
 
-  // Connect inbound caller to conf
-  // Create outbound dial to desired PSTN number
-  // when that call answers, add to conf
-  // save conf-id
-  // every {duration} play audio to conf-id
+    // Connect inbound caller to conf
+    // Create outbound dial to desired PSTN number
+    // when that call answers, add to conf
+    // save conf-id
+    // every {duration} play audio to conf-id
 
-  const response = new VoiceResponse();
-  response.dial(
-    {
-      callerId: `${userRecord?.telnyxPhoneNumber}`,
-      record: "record-from-answer-dual",
-      recordingStatusCallback: "recordStatus",
-      action: "dialFinished",
-      method: "POST",
-    },
-    `+${phoneNumber}`
-  );
-  res.type("application/xml");
-  res.send(response.toString());
-});
-
-router.route("/recordFinished").post(async (req, res) => {
-  const event = req.body as Telnyx.events.CallRecordingSavedEvent;
-  console.log(event);
-  res.type("application/xml");
-  res.send(texml.hangupTeXML(hangupSentence));
-});
-
-router.route("/recordStatus").post(async (req, res) => {
-  const event = req.body as Telnyx.events.CallRecordingSavedEvent;
-  console.log(event);
-  res.sendStatus(200);
-});
+    res.type("application/xml");
+    res.send(
+      texml.dialTeXML(
+        `${userRecord?.telnyxPhoneNumber}`,
+        "/dialFinished",
+        "POST",
+        "record-from-answer-dual",
+        "recordFinished",
+        `+${phoneNumber}`
+      )
+    );
+  });
 
 router.route("/dialFinished").post(async (req, res) => {
   const event = req.body;
   console.log(event);
   res.sendStatus(200);
+});
+
+router.route("/recordFinished").post(async (req, res) => {
+  const event = req.body;
+  console.log(event);
+  res.type("application/xml");
+  res.send(texml.hangupTeXML("Thank you for the call, hanging up"));
 });
 
 export default router;
--- express-texml-call-mask/packages/texml.ts
+++ express-texml-call-mask/packages/texml.ts
@@ -1,5 +1,43 @@
 import xmlFormat from "xml-formatter";
 
+/**
+ * https://developers.telnyx.com/docs/voice/programmable-voice/texml-translator#gather
+ */
+export const gatherTeXML = (
+  gatherPrompt: string,
+  finishOnKey: string,
+  minDigits: number,
+  maxDigits: number
+) =>
+  xmlFormat(`<?xml version="1.0" encoding="UTF-8"?>
+  <Response>
+    <Gather action="gather" finishOnKey="${finishOnKey}" minDigits="${minDigits}" maxDigits="${maxDigits}">
+      <Say voice="alice">${gatherPrompt}</Say>
+    </Gather>
+    <Say>Thank you for the call, hanging up</Say>
+  </Response>`);
+
+/**
+ * https://developers.telnyx.com/docs/voice/programmable-voice/texml-translator#dial
+ */
+export const dialTeXML = (
+  callerId: string,
+  action: string,
+  method: string,
+  phoneNumber: string,
+  record: string,
+  recordingStatusCallback: string
+) =>
+  xmlFormat(`<?xml version="1.0" encoding="UTF-8"?>
+  <Response>
+    <Dial callerId="${callerId}"  action="${action}" method="${method}" record="${record}" recordingStatusCallback="${recordingStatusCallback}">
+      <Number>${phoneNumber}</Number>
+    </Dial>
+  </Response>`);
+
+/**
+ * https://developers.telnyx.com/docs/voice/programmable-voice/texml-translator#hangup
+ */
 export const hangupTeXML = (hangupSentence: string) =>
   xmlFormat(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
